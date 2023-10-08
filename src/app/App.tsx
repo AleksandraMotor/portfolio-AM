4696fac4a053b0d9d717310b931afbe8f06cbdc1
AleksandraMotor <aleksandramotor@gmail.com>
2023-10-08 03:50:02 +0200

    change path for production deploy

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,11 +13,11 @@ const App: React.FunctionComponent = () => {
     <div className="app">
       <BrowserRouter>
             <Routes>
-              <Route path='/' element={<Layout />}>
+              <Route path='portfolio-AM/' element={<Layout />}>
                 <Route index element={<Home />} />
-                <Route path='skills' element={<Skills />} />
-                <Route path='projects' element={<Projects />} />    
-                <Route path='contact' element={<Contact />} /> 
+                <Route path='portfolio-AM/skills' element={<Skills />} />
+                <Route path='portfolio-AM/projects' element={<Projects />} />    
+                <Route path='portfolio-AM/contact' element={<Contact />} /> 
                 <Route path='*' element={<Contact />} /> 
               </Route>
             </Routes>
